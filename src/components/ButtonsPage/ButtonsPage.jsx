297f258e15bf5a3dbfd1cd37bf2392be96007b96
jamesknelson <james@jamesknelson.com>
2015-07-05 16:57:40 +0900

    more button examples

--- src/components/ButtonsPage/ButtonsPage.jsx
+++ src/components/ButtonsPage/ButtonsPage.jsx
@@ -1,4 +1,5 @@
 import "./ButtonsPage.less"
+
 import React from "react"
 import NUI,
        {Base,
@@ -7,9 +8,22 @@ import NUI,
         FloatingActionButton,
         FloatingActionButtonExpander,
         IconButton,
+        IconMenu,
+        MenuDivider,
+        MenuItem,
         RaisedButton} from "numbat-ui"
 
 export default class ButtonsPage extends Base {
+  static propTypes = {
+    onToggleAppMenu: React.PropTypes.func.isRequired,
+  }
+
+
+  onSelectMenuItem(value) {
+    console.log(value, "menu item selected")
+  }
+
+
   render() {
     let counter = 1
 
@@ -48,6 +62,47 @@ export default class ButtonsPage extends Base {
             <FloatingActionButton iconType="edit" />
             <FloatingActionButton iconType="F" />
           </FloatingActionButtonExpander>
+        </div>
+
+        <div>
+          <div style={{display: 'inline-block', marginRight: '15px'}}>
+            <IconButton iconType="edit" tooltip="Edit" tabIndex={counter++} type="light" />
+            <IconButton iconType="star" tooltip="Set as favourite" tabIndex={counter++} type="light" disabled />
+            <IconButton iconType="details-menu" tooltip="Other Options" tabIndex={counter++} type="light" />
+          </div>
+          <div style={{display: 'inline-block', marginRight: '15px'}}>
+            <IconButton iconType="edit" tooltip="Edit" tabIndex={counter++} type="light-primary" />
+            <IconButton iconType="star" tooltip="Set as favourite" tabIndex={counter++} type="light-primary" disabled />
+            <IconButton iconType="details-menu" tooltip="Other Options" tabIndex={counter++} type="light-primary" />
+          </div>
+          <div style={{display: 'inline-block', marginRight: '15px'}}>
+            <IconButton iconType="edit" tooltip="Edit" tabIndex={counter++} type="light-accent" />
+            <IconButton iconType="star" tooltip="Set as favourite" tabIndex={counter++} type="light-accent" disabled />
+            <IconButton iconType="details-menu" tooltip="Other Options" tabIndex={counter++} type="light-accent" />
+          </div>
+        </div> 
+
+        <div style={{backgroundColor: "#333333"}}>
+          <div style={{display: 'inline-block', marginRight: '15px'}}>
+            <IconButton iconType="edit" tooltip="Edit" tabIndex={counter++} type="dark" />
+            <IconButton iconType="star" tooltip="Set as favourite" tabIndex={counter++} type="dark" disabled />
+            <IconButton iconType="details-menu" tooltip="Other Options" tabIndex={counter++} type="dark" />
+          </div>
+          <div style={{display: 'inline-block', marginRight: '15px'}}>
+            <IconButton iconType="edit" tooltip="Edit" tabIndex={counter++} type="dark-primary" />
+            <IconButton iconType="star" tooltip="Set as favourite" tabIndex={counter++} type="dark-primary" disabled />
+            <IconButton iconType="details-menu" tooltip="Other Options" tabIndex={counter++} type="dark-primary" />
+          </div>
+          <div style={{display: 'inline-block', marginRight: '15px'}}>
+            <IconButton iconType="edit" tooltip="Edit" tabIndex={counter++} type="dark-accent" />
+            <IconButton iconType="star" tooltip="Set as favourite" tabIndex={counter++} type="dark-accent" disabled />
+            <IconMenu iconType="details-menu" tabIndex={counter++} type="dark-accent" onSelectItem={this.onSelectMenuItem}>
+              <MenuItem label="Edit photo" value="edit" />
+              <MenuItem label="View details" value="contact" />
+              <MenuDivider />
+              <MenuItem label="Delete" value="delete" />
+            </IconMenu>
+          </div>
         </div>
       </div>
     )
